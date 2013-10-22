add99f9e5d21c24072e11d6c001f66b188085a57
BleedObsidian <bleedobsidian@gmail.com>
2013-10-23 00:15:13 +0100

    Created basic framework.

---
+++ src/main/java/com/gmail/bleedobsidian/areaprotect/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2013 Jesse Prescott <BleedObsidian@gmail.com>
+ *
+ * AreaProtect is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/gpl.html>.
+ */
+
+package com.gmail.bleedobsidian.areaprotect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public String getVersion() {
+        return this.getDescription().getVersion();
+    }
+}
