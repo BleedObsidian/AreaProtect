43bbf8dd8d6e1a4067d70444eef1d263e2d95d9f
BleedObsidian <bleedobsidian@gmail.com>
2013-10-23 14:20:04 +0100

    Added configuration files and implemented basic framework.

--- src/main/java/com/gmail/bleedobsidian/areaprotect/Main.java
+++ src/main/java/com/gmail/bleedobsidian/areaprotect/Main.java
@@ -17,17 +17,80 @@
 
 package com.gmail.bleedobsidian.areaprotect;
 
+import java.io.IOException;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.gmail.bleedobsidian.areaprotect.configuration.ConfigFile;
+import com.gmail.bleedobsidian.areaprotect.logger.PluginLogger;
+
 public class Main extends JavaPlugin {
+    private ConfigFile config;
+    private ConfigFile groups;
+
+    private Vault vault;
+
     @Override
     public void onEnable() {
+        // Setup Logger
+        PluginLogger.setJavaPlugin(this);
 
+        // Load Configuration Files
+        try {
+            this.config = new ConfigFile("config.yml");
+            this.groups = new ConfigFile("groups.yml");
+
+            this.config.load(this);
+            this.groups.load(this);
+        } catch (IOException e) {
+            PluginLogger.error("Failed to load configuration files.", e);
+            return;
+        }
+
+        // Set Language
+        String locale = this.config.getFileConfiguration().getString("Locale");
+
+        if (Language.exists(locale)) {
+            Language.setLangauge(locale + ".yml", this);
+        } else {
+            PluginLogger.warning("Failed to find locale: " + locale
+                    + ", using en-us instead.", true);
+            Language.setLangauge("en-us.yml", this);
+        }
+
+        // Check For Update
+        if (this.config.getFileConfiguration().getBoolean(
+                "Updates.Check-For-Update")) {
+            if (Update.isNewVersionAvailable(this)) {
+                PluginLogger.warning(Language.getLanguageFile().getMessage(
+                        "Console.Update-Available"));
+            }
+        }
+
+        // Load Vault
+        if (this.config.getFileConfiguration().getBoolean("Use-Vault")) {
+            this.vault = new Vault();
+
+            if (this.vault.load(this)) {
+                PluginLogger.info(Language.getLanguageFile().getMessage(
+                        "Console.Vault.Successful"));
+            } else {
+                PluginLogger.warning(
+                        Language.getLanguageFile().getMessage(
+                                "Console.Vault.Unsuccessful"), true);
+            }
+        }
+
+        PluginLogger.info(Language.getLanguageFile().getMessage(
+                "Console.Enabled",
+                new String[] { "%Version%", this.getVersion() }));
     }
 
     @Override
     public void onDisable() {
-
+        PluginLogger.info(Language.getLanguageFile().getMessage(
+                "Console.Disabled",
+                new String[] { "%Version%", this.getVersion() }));
     }
 
     public String getVersion() {
